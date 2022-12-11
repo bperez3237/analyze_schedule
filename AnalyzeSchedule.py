e2ca8969575d9ab514eaebb9fbb406f4d8530988
brian <b.perez3237@gmail.com>
2022-12-11 16:30:28 -0500

    Initial commit:

---
+++ AnalyzeSchedule.py
@@ -0,0 +1,11 @@
+﻿from numpy import number
+import xlsxwriter as xl
+import pandas as pd
+from datetime import date
+import pprint as pp
+import json
+from formats import *
+
+cost_rprt_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Contract Forecasting Spreadsheet\Period 12 Export 12.09.22.xlsx')
+cost_rprt = pd.read_excel(cost_rprt_xls)
+
